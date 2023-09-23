d95be7f39cbff637f04987d09c267a194c862380
ApostL78 <ayupovart75@mail.ru>
2023-09-23 13:03:05 +0400

    robots creation excel report (task 2)

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+from orders.utils import _get_robot_data_last_week, _create_excel_file
+
+
+class ExcelReportView(View):
+    def get(self, request, *args, **kwargs):
+        robots = _get_robot_data_last_week()
+        output = _create_excel_file(robots)
+
+        response = HttpResponse(
+            content_type="application/vnd.openxmlformats-officedocument"
+            ".spreadsheetml.sheet"
+        )
+        response["Content-Disposition"] = "attachment; filename=robot_report.xlsx"
+        response.write(output.getvalue())
+        return response
